541995c06e703915e5a3103da27300bb6e1bd0b9
dirkp <dirkp@SURFBKDP>
2016-12-09 06:25:26 +0100

    HelloWorld

---
+++ Testproject/HelloWorld/HelloWorldTests/HelloWorldTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 aNewForm = new Form1();
+            Assert.IsNotNull(aNewForm);
+        }
+    }
+}
